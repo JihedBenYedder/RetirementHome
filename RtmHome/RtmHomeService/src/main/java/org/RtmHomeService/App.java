8cbbc45aa03281c612ec0d33e132abc17fc42419
Anis <anis@MacBook-Pro-de-Anis.local>
2016-11-22 19:41:01 +0100

    add Modules to project

---
+++ RtmHome/RtmHomeService/src/main/java/org/RtmHomeService/App.java
@@ -0,0 +1,13 @@
+package org.RtmHomeService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
