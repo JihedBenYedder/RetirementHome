1c00db6e6649336784e9861a2b28d519e1484148
Anis <anis@MacBook-Pro-de-Anis.local>
2016-11-23 00:41:36 +0100

    fixing_commit_new_model

---
+++ RtmHome/RtmHomeModel/src/main/java/com/rtmhome/Guarantor.java
@@ -0,0 +1,26 @@
+package com.rtmhome;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ * @author Zhlel
+ *
+ */
+@Entity
+@Table(name = "TRUSTED_PERSON")
+public class Guarantor extends Person {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ID_TRUSTED_PERSON", unique = true, nullable = true)
+	private Long idTrustedPerson;
+
+	@Column(name = "RELATIONSHIP", nullable = true)
+	private String relationShip;
+
+}
