53ace062228d1d9c770593487d9b62a5b148e85b
z <z@MacBook-Pro-de-z.local>
2016-11-22 18:05:04 +0100

    first commit

---
+++ RetirementHome/src/main/java/RetirementHome/RetirementHome/App.java
@@ -0,0 +1,13 @@
+package RetirementHome.RetirementHome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
