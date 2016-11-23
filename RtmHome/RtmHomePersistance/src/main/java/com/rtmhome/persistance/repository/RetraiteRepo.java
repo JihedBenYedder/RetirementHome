a20938179a82eea12307974221443b9ad0e663d8
Anis <anis@MacBook-Pro-de-Anis.local>
2016-11-23 16:07:51 +0100

    Repo&AppContext

--- RtmHome/RtmHomePersistance/src/main/java/com/rtmhome/persistance/repository/RetraiteRepo.java
+++ RtmHome/RtmHomePersistance/src/main/java/com/rtmhome/persistance/repository/RetraiteRepo.java
@@ -1,14 +1,13 @@
-package com.rtmhome.persistance;
+package com.rtmhome.persistance.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.rtmhome.model.RetirementHome;
 import com.rtmhome.model.Retraite;
 
-public interface RetraiteRepo extends CrudRepository<Retraite, Long> {
+public interface RetraiteRepo extends CrudRepository<Retraite, Long>{
 
 	public Retraite findByIdRetraite(Long idRetraite);
-	
-	
 	
 	
 }
