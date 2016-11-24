eff1f265231b70ccddc65c2bdac4cb2e125f9c4c
z <z@MacBook-Pro-de-z.local>
2016-11-24 16:24:40 +0100

    merge

--- RtmHome/RtmHomePersistance/src/main/java/com/rtmhome/persistance/repository/RetraiteRepo.java
+++ RtmHome/RtmHomePersistance/src/main/java/com/rtmhome/persistance/repository/RetraiteRepo.java
@@ -1,11 +1,14 @@
 package com.rtmhome.persistance.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rtmhome.model.RetirementHome;
 import com.rtmhome.model.Retraite;
 
-public interface RetraiteRepo extends CrudRepository<Retraite, Long>{
+
+public interface RetraiteRepo extends JpaRepository<Retraite, Long>{
 
 	public Retraite findByIdRetraite(Long idRetraite);
 	
